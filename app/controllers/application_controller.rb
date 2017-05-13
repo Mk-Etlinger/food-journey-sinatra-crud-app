a5476e64326fb3f92eb65f3649ba981db6ffa9a1
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-13 12:15:27 -0400

    corrected redirects to go to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    redirect("/tweets") if logged_in?(session)
+    redirect("/dashboard") if logged_in?(session)
 
     haml :'registration/signup'
   end
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
 
     if @user.save
       session[:id] = @user.id
-      redirect "/tweets"
+      redirect "/dashboard"
     else
       redirect "/signup"
     end
@@ -51,61 +51,6 @@ class ApplicationController < Sinatra::Base
     session.clear
     redirect "/login"
   end
-  
-  get '/tweets' do
-    @tweets = Tweet.all
-    @user = current_user(session)
-    if logged_in?(session)
-      erb :"tweets/index"
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/tweets/new' do
-    redirect("/login") if !logged_in?(session)
-    erb :'tweets/new'
-  end
-
-  post '/tweets/new' do
-    redirect back if params[:content].empty?
-    @tweet = Tweet.create(params)
-    @tweet.user = current_user(session)
-    @tweet.save
-    redirect "/tweets"
-  end
-
-  get '/tweets/:id' do
-    redirect("/login") if !logged_in?(session)
-    @tweet = Tweet.find(params[:id])
-    @current_user = current_user(session)
-    erb :'tweets/show'
-  end
-
-  delete '/tweets/:id/delete' do
-    @tweet = Tweet.find(params[:id])
-    @tweet.delete if current_user(session).id == @tweet.user.id
-    redirect "/tweets"
-  end
-
-  get '/tweets/:id/edit' do
-    redirect("/login") if !logged_in?(session)
-    @tweet = Tweet.find(params[:id])
-    erb :'tweets/edit'
-  end
-
-  patch '/tweets/:id/edit' do
-    redirect back if params[:content].empty?
-    @tweet = Tweet.find(params[:id])
-    @tweet.update(content: params[:content])
-    redirect "/tweets/#{@tweet.id}"
-  end
-
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'users/index'
-  end
-
 
   helpers do
     def logged_in?(session)
