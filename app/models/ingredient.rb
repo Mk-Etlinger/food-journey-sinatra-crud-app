f195b05c9d7fd5b2795e45b5e810bb92cad4a19b
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-16 17:25:05 -0400

    removed parse_ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,11 @@ class Ingredient < ActiveRecord::Base
   has_many :meals, through: :meal_ingredients
 
   validates_presence_of :name
+
+  def self.parse_ingredients(new_ingredients)
+    binding.pry
+    new_ingredients.split(",").each do |ingredient|
+      @meal.ingredients << self.find_or_create_by(name: ingredient.strip)
+    end
+  end
 end
