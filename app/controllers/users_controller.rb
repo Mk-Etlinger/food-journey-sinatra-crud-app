ed7dffbb6c5145faf918c9d66494185401d50049
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-15 23:14:22 -0400

    added session.clear to /logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
 
   get '/dashboard/:user' do
+    redirect("/login") if !logged_in?(session)
     @user = User.find_by(username: params[:user])
     haml :'users/dashboard'
   end
   
   get '/logout' do
-
+    session.clear
     haml :index
   end
   
