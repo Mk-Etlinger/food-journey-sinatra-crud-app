a8f32f51cfd7ad2b673eaded1df84719f3d8b095
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-16 17:25:11 -0400

    added parse_ingredients instance method

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,6 +5,13 @@ class Meal < ActiveRecord::Base
 	has_many :ingredients, through: :meal_ingredients
 
 	# validates_presence_of :date_time
+
+	def parse_ingredients(new_ingredients)
+    new_ingredients.split(",").each do |ingredient|
+      self.ingredients << Ingredient.find_or_create_by(name: ingredient.strip)
+    end
+  end
+
 end
 
 
