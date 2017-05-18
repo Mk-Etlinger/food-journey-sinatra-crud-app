9a96693fdec57a62ab6878bc5f534d059c986ce7
ETphonehomeslice <mk.etlinger@gmail.com>
2017-05-18 11:52:33 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
     has_many :meals
     has_secure_password
-    validates_presence_of :username, :email
+    validates_presence_of :password
+    validates :username,  uniqueness: true
+    validates :email, uniqueness: true
+    validates :username, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 
     def slug
         self.username.gsub(" ", "-")    
