9af1a75c9763beaebd90356ad554b30338062281
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-10 13:50:04 -0400

    formatted whitespace

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,7 @@
 class Ingredient < ActiveRecord::Base
-
   has_many :meal_ingredients
   has_many :meals, through: :meal_ingredients
 
   validates_presence_of :name
   validates :name, format: { with: /[a-zA-Z,]/ }
-  
 end
