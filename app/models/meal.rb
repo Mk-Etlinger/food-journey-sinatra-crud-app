768309506e0a4d85563709c88124d5297fdcadd6
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-10 12:10:24 -0400

    cleaned up format

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,20 +1,14 @@
 class Meal < ActiveRecord::Base
-  
-	has_many :meal_ingredients
-	belongs_to :user
-	has_many :ingredients, through: :meal_ingredients
+  has_many :meal_ingredients
+  belongs_to :user
+  has_many :ingredients, through: :meal_ingredients
 
-	validates_presence_of :meal_type, :description
-	validates :description, length: { maximum: 30 }
-	
+  validates_presence_of :meal_type, :description
+  validates :description, length: { maximum: 30 }
 
-	def parse_ingredients(new_ingredients)
-    new_ingredients.split(",").each do |ingredient|
-      self.ingredients << Ingredient.find_or_create_by(name: ingredient.strip)
+  def parse_ingredients(new_ingredients)
+    new_ingredients.split(',').each do |ingredient|
+      ingredients << Ingredient.find_or_create_by(name: ingredient.strip)
     end
   end
-
 end
-
-
-
