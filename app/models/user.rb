f2df2dc4c9872d165971b141c72df1d4e2902b6e
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-10 13:50:18 -0400

    formatted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
-    has_many :meals
-    has_secure_password
-    validates_presence_of :password
-    validates :username,  uniqueness: true
-    validates :email, uniqueness: true
-    validates :username, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
-    validates :password, length: { in: 6..20 }
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-
+  has_many :meals
+  has_secure_password
+  validates_presence_of :password
+  validates :username,  uniqueness: true
+  validates :email, uniqueness: true
+  validates :username, format: { with: /\A[a-zA-Z]+\z/,
+                                 message: 'only allows letters' }
+  validates :password, length: { in: 6..20 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
